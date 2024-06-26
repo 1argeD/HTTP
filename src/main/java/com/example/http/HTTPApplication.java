4faf775d4a04857c62df3fabc8e447b811ac15d1
1argeD <94172354+1argeD@users.noreply.github.com>
2024-06-26 16:36:03 +0900

    <feat>ActiveMQ 추가 , X.Open XA 프로토콜, JPA 분산트랜잭션

--- src/main/java/com/example/http/HTTPApplication.java
+++ src/main/java/com/example/http/HTTPApplication.java
@@ -1,10 +1,22 @@
 package com.example.http;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.servlet.http.HttpSession;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +49,67 @@ class SessionController {
         m.put("instance_ip", this.ip);
         m.put("uuid",uid.toString());
         return m;
+    }
+}
+
+interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
+
+@Entity
+class Account {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String username;
+
+    public Account() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Account(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+
+}
+
+@Component
+class Message {
+    private Log log = LogFactory.getLog(getClass());
+
+    @JmsListener(destination = "accounts")
+    public void onMessage(String content){
+          log.info("-------> "+content);
+    }
+}
+
+@Service
+@Transactional
+class AccountService{
+    private JmsTemplate jmsTemplate;
+
+    private AccountRepository accountRepository;
+
+    public void createAccountAndNotify(String username) {
+        this.jmsTemplate.convertAndSend("account",username);
+        this.accountRepository.save(new Account(username));
+        if("error".equals(username)) {
+            throw new RuntimeException("Simulate Error");
+        }
     }
 }
